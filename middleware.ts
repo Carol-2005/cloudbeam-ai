27c27f35d3f63be6e3b449767404c868c286d6e7
carol <carol.lobo2005@gmail.com>
2025-06-14 16:33:48 +0530

    Middleware established

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,40 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+//the document available on clark for middleware.ts will protect all the pages but we want customization
+import { NextResponse } from 'next/server';
+//isPublic(req) will return true if path matches 
+const isPublicRoute=createRouteMatcher([
+    '/signup',
+    '/signin',
+    '/',
+    '/home'
+])
 
-export default clerkMiddleware();
+const isPublicApiRoute=createRouteMatcher([
+    '/api/videos'
+])
+//clerk middleware is used to have custome made redirections
+export default clerkMiddleware(async(auth,req)=>{
+   const {userId}=await auth();
+   //req.url---https://localhost:3000/home
+   const currentUrl=new URL(req.url);
+   const isHomePage=currentUrl.pathname==='/home';
+   const isApiRequest=currentUrl.pathname.startsWith('/api');
 
+   if(userId  && isPublicRoute(req) && !isHomePage){
+    return NextResponse.redirect(new URL('/home',req.url))
+   }
+   //if not logged in
+   if(!userId){
+    if(!isPublicApiRoute(req) && !isPublicRoute(req)){
+        return NextResponse.redirect(new URL('/signin',req.url));
+        //this will craete new url using the current request base url
+    }
+        if(isApiRequest && !isPublicRoute(req)){
+            return NextResponse.redirect(new URL('/signin',req.url));
+        }
+    
+   }
+});
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
